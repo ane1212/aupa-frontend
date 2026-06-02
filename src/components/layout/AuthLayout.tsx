eeee9ee27fc5d10a2dae171700402116560a4635
r3dc0m <jonathanvelez85@gmail.com>
2026-06-02 13:37:58 +0200

    0602.1337 previo a conector, agrego layout sin navbar para pantallas de login y register

---
+++ src/components/layout/AuthLayout.tsx
@@ -0,0 +1,14 @@
+// components/layout/AuthLayout.tsx
+import { Outlet } from "react-router-dom";
+
+const AuthLayout = () => {
+    return (
+        <div className="auth">
+            <main className="auth-main">
+                <Outlet />
+            </main>
+        </div>
+    );
+};
+
+export default AuthLayout;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,21 +1,28 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
-import { Error, Experiences, Home, Login, Nearby, Profile, Register, Saved } from '../pages';
-import Root from '../components/layout/Root';
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import { Error, Experiences, Home, Login, Nearby, Profile, Register, Saved } from "../pages";
+import Root from "../components/layout/Root";
+import AuthLayout from "../components/layout/AuthLayout";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Root />,
     errorElement: <Error />,
     children: [
       { index: true, element: <Navigate to="/home" /> },
-      { path: 'home', element: <Home /> },
-      { path: 'nearby', element: <Nearby /> },
-      { path: 'experiences', element: <Experiences /> },
-      { path: 'saved', element: <Saved /> },
-      { path: 'login', element: <Login /> },
-      { path: 'register', element: <Register /> },
-      { path: 'profile', element: <Profile /> },
+      { path: "home", element: <Home /> },
+      { path: "nearby", element: <Nearby /> },
+      { path: "experiences", element: <Experiences /> },
+      { path: "saved", element: <Saved /> },
+      { path: "profile", element: <Profile /> },
+    ],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
     ],
   },
 ]);
