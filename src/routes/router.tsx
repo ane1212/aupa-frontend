af70c6070f745b371243ae842fcabe62805dfa1e
r3dc0m <jonathanvelez85@gmail.com>
2026-06-04 13:48:49 +0200

    0604.1348 borrado archivo test y cambiado nombre de variable Error a ErrorPage.

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import { Detail, Error, Experiences, Home, Login, Nearby, Profile, Register, Saved } from "../pages";
+import { Detail, ErrorPage, Experiences, Home, Login, Nearby, Profile, Register, Saved } from "../pages";
 import Root from "../components/layout/Root";
 import AuthLayout from "../components/layout/Auth";
 import AuthenticatedRoute from "../components/layout/AuthenticatedRoute";
@@ -8,7 +8,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <Error />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Navigate to="/home" /> },
       { path: "home", element: <Home /> },
